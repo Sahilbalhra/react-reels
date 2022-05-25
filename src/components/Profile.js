e3265ce0dd957e6f1885884efbfadffc63a70c53
Sahilbalhra <sahilbalhra0@gmail.com>
2022-05-25 22:42:43 +0530

    signup added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Box } from "@mui/material";
+import "./Profile.css";
 
 const Profile = () => {
   return (
-    <div>Profile</div>
-  )
-}
+    <>
+      <Box component="div" className="header"></Box>
+      <Box component="div" className="main">
+        <Box component="img" src="http://via.placeholder.com/640x360" alt="" className="pimg"/>
+        <Box component="div" className="details">
+          <Box component="div" className="content">
+            Name
+          </Box>
+          <Box component="div" className="content">
+            No Of Posts: <span>Posts</span>
+          </Box>
+          <Box component="div" className="content">
+            Email<span>email@gmail.com</span>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
+};
 
-export default Profile
+export default Profile;
