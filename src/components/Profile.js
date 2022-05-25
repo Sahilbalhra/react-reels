35947557599502882351477de784de499c4e5d82
Sahilbalhra <sahilbalhra0@gmail.com>
2022-05-25 23:08:50 +0530

    profile sample added

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -7,16 +7,23 @@ const Profile = () => {
     <>
       <Box component="div" className="header"></Box>
       <Box component="div" className="main">
-        <Box component="img" src="http://via.placeholder.com/640x360" alt="" className="pimg"/>
+        <Box component="div" className="pimg__container">
+          <Box
+            component="img"
+            src="http://via.placeholder.com/640x360"
+            alt=""
+            className="pimg"
+          />
+        </Box>
         <Box component="div" className="details">
           <Box component="div" className="content">
             Name
           </Box>
           <Box component="div" className="content">
-            No Of Posts: <span>Posts</span>
+            No Of Posts: <span className="bold_text">Posts</span>
           </Box>
           <Box component="div" className="content">
-            Email<span>email@gmail.com</span>
+            Email: <span className="bold_text">email@gmail.com</span>
           </Box>
         </Box>
       </Box>
